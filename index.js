6f46f69d5a12c8dd617f016109fd03a28a5337aa
Yash Gupta <yg123yash@gmail.com>
2020-01-15 18:12:35 +0530

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 app.listen(PORT, () => {
